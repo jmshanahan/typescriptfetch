f13bbe1ee9ea7795ffb386cc8c58525831579498
jmshanahan <comeraghsolutions@gmail.com>
2023-03-31 11:24:16 +1100

    Posted data to firebase

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Introduction
 
 A simple app fetching data with typscript and using class transformer.
+
+The class transformer has been taken out when we started to get data from firebase.
+
+Firebase returns an object of objects with keys.
+SWAPI returns an array of objects
+
+You need to note the difference.
+If you get objects with keys you will need to transform that into an array of objects as that is the
+most convient way to manage things within react.
+
+```
+      const loadedMovies: Array<IMovie> = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+```
--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,19 @@ function App() {
   const [movies, setMovies] = useState<Array<IMovie>>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const addMovieHandler = (movie: IOmitIdIAddMovie) => {
+  const addMovieHandler = async (movie: IOmitIdIAddMovie) => {
     console.log(movie);
+    const url =
+      "https://react-docs-361db-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json";
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
   };
 
   const fetchMoviHandler = useCallback(async () => {
@@ -21,14 +32,44 @@ function App() {
     setError(null);
 
     try {
-      const url = "https://swapi.dev/api/films/";
+      const url =
+        "https://react-docs-361db-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json";
       const response = await fetch(url);
+
       if (!response.ok) {
         throw new Error("Something went wrong");
       }
-      const { results } = await response.json();
-      const movies = plainToInstance(FetchedMovie, results);
-      setMovies(movies);
+      const data = await response.json();
+
+      const loadedMovies: Array<IMovie> = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      loadedMovies.forEach((profile, index, arr) => {
+        console.log(
+          `Index: ${index}, id: ${profile.id}, Title: ${profile.title}`
+        );
+      });
+      for (const key in loadedMovies) {
+        console.log(loadedMovies[key].title);
+      }
+
+      console.log(data);
+      // Use this method if you are using the SWAPI
+      // It didnt work for firebase.
+
+      // NOTE: firbase returns an object with embedded objects
+      // SWAPI returns an array of objects. Hence different methods were required.
+
+      // const { results } = await response.json();
+      // const movies = plainToInstance(FetchedMovie, results);
+
+      setMovies(loadedMovies);
     } catch (err: any) {
       setError(err.message);
     }
@@ -43,12 +84,13 @@ function App() {
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />;
   }
-  if (error) {
-    content = <p>{error}</p>;
-  }
-  if (isLoading) {
-    content = <p>Is Loading ...</p>;
-  }
+  // if (error) {
+  //   content = <p>{error}</p>;
+  // }
+  // if (isLoading) {
+  //   content = <p>Is Loading ...</p>;
+  // }
+  console.log(`The content is ${content}`);
 
   return (
     <React.Fragment>
